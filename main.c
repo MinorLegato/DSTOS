f6697fa9628402fef3df70767e3f8f39b0c6817f
MinorLegato <anton.siderud@gmail.com>
2017-02-23 11:39:34 +0100

    FLUX2

--- main.c
+++ main.c
@@ -1,35 +1,13 @@
 // main.c
 #include "kernel.h"
 
-TCB taskA;
 TCB * Running;
 
-void task1(void);
-
-i32 tickCounter = 0;
-
-void TimerInt (void)
+void TimerInt(void)
 {
-    tickCounter++;
-    //Check the Timerlist for tasks that are ready for
-    //execution, move these to Readylist
-    //Check the Waitinglist for tasks that have expired
-    //deadlines, move these to Readylist and clean up
-    //their Mailbox entry.
 }
 
 int main(void)
 {
-	//Running = &taskA;
-	//Running->PC = task1;
-	//Running->SP = &(Running->StackSeg[STACK_SIZE-1]);
-    
-	//LoadContext();
-
-    while (1) {
-        SaveContext();
-        TimerInt();
-        LoadContext();
-    }
+return 1;
 }
-

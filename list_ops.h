512de614d626afebeb5af5830bef9842f9bba05b
Tobias9507 <tobhan15@student.hh.se>
2017-08-20 14:44:59 +0200

    Revert "bajsHOOOOOOOOOR"
    This reverts commit 2daf725c9b47ad63f3abedbc7f0f22bb4ddfe5ea.

--- list_ops.h
+++ list_ops.h
@@ -3,7 +3,6 @@
 
 #include "kernel.h"
 #include "string.h"
-#include "kernel_data.h"
 
 // =============================== TASK LIST API ===================================== //
 
@@ -16,8 +15,8 @@ typedef listobj TaskNode;
 static TaskNode* allocTask(void (*body)(), u32 d) {
     TaskNode* task = NULL;
     TCB*      tcb  = NULL;
-    if (task = alloc(sizeof *task), !task) { return NULL; }
-    if (tcb  = alloc(sizeof *task), !tcb)  { free(task); return NULL; }
+    if (task = calloc(1, sizeof *task), !task) { return NULL; }
+    if (tcb  = calloc(1, sizeof *task), !tcb)  { free(task); return NULL; }
     tcb->DeadLine = d;
     tcb->PC = body;
     tcb->SP = tcb->StackSeg;
@@ -26,14 +25,12 @@ static TaskNode* allocTask(void (*body)(), u32 d) {
 }
 
 static void initTaskList(TaskList* taskList) {
-    taskList->pHead = alloc(sizeof (TaskNode));
-    taskList->pTail = alloc(sizeof (TaskNode));
-    taskList->pHead->pNext = taskList->pTail;
-    taskList->pTail->pPrevious = taskList->pHead;
+    taskList->pHead = NULL;
+    taskList->pTail = NULL;
 }
 
 static int noTasks(TaskList* taskList) {
-    return taskList->pHead->pNext == taskList->pTail;
+    return taskList->pHead == taskList->pTail;
 }
 
 static void insertTask(TaskNode* newNode, TaskNode* prev, TaskNode* next) {
@@ -43,19 +40,44 @@ static void insertTask(TaskNode* newNode, TaskNode* prev, TaskNode* next) {
     prev->pNext = newNode;
 }
 
-static void addTask_Deadline(TaskList* taskList, TaskNode* newNode){
+static void addTask_First(TaskList* taskList, TaskNode* newNode) {
     if (!newNode) { return; }
-    TaskNode* temp = taskList->pHead;
-    while (temp->pNext != taskList->pTail
-           && newNode->pTask->DeadLine > temp->pNext->pTask->DeadLine){
-        temp = temp->pNext;
+    if (taskList->pHead == NULL) {
+        taskList->pHead = newNode;
+        taskList->pTail = newNode;
+    } else {
+        insertTask(newNode, taskList->pHead, taskList->pHead->pNext);
     }
-    insertTask(newNode, temp, temp->pNext);
+}
+
+static void addTask_Deadline(TaskList* taskList, TaskNode* newNode) {
+    if (!newNode) { return; }
+    if (taskList->pHead == NULL) {
+        taskList->pHead = newNode;
+        taskList->pTail = newNode;
+    } else {
+        TaskNode* temp = taskList->pHead;
+        while (temp->pNext != taskList->pTail 
+            && newNode->pTask->DeadLine > temp->pNext->pTask->DeadLine){
+            temp->pNext = temp->pNext->pNext;
+        }
+        insertTask(newNode, temp, temp->pNext);
+    }
 }
 
 static void addTask_nTCnt(TaskList* taskList, TaskNode* newNode) {
     if (!newNode) { return; }
-    
+    if (taskList->pHead == NULL) {
+        taskList->pHead = newNode;
+        taskList->pTail = newNode;
+    } else {
+        TaskNode* temp = taskList->pHead;
+        while (temp->pNext != taskList->pTail 
+            && newNode->nTCnt > temp->pNext->nTCnt){
+            temp->pNext = temp->pNext->pNext;
+        }
+        insertTask(newNode, temp, temp->pNext);
+    }
 }
 
 static void clearTasks(TaskList* head) {

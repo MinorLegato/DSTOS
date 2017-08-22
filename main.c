94b6b79db181a8d74f61f5a1ccdcb758a9ca91b3
MinorLegato <anton.siderud@gmail.com>
2017-08-22 13:16:08 +0200

    notest

--- main.c
+++ main.c
@@ -21,31 +21,10 @@ void isr_on()  {}
 void TimerInt(void) { tickCounter++; }
 
 int main(void) {
-    mailbox* mBox = create_mailbox(100, 100);
-
-    if (no_messages(mBox)) { printf("\nEMPTY\n"); }
-
-    msgPushBack(mBox, createMsg("1!\n"));
-    msgPushBack(mBox, createMsg("2!\n"));
-    msgPushBack(mBox, createMsg("3!\n"));
-    msgPushBack(mBox, createMsg("4!\n"));
-    msgPushBack(mBox, createMsg("5!\n"));
-
-    if (no_messages(mBox)) { printf("\nEMPTY\n"); }
-
-    msg* m;
-    forEach(m, mBox) { printf(m->pData); }
-
-    msgPopFront(mBox);
-    msgPopBack(mBox);
-
-    printf("\n\n");
-    forReve(m, mBox) { printf(m->pData); }
-
-    if (no_messages(mBox)) { printf("\nEMPTY\n"); }
-
+    
+    
     printf("\nDONE");
-
+    
     /*
     init_kernel();
     run();

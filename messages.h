c283f6baf5a8e583ebf85a865c8d4c87ccabc350
Tobias9507 <tobhan15@student.hh.se>
2017-08-25 18:09:28 +0200

    MIGHTBEEEEEEDONE

--- messages.h
+++ messages.h
@@ -29,7 +29,7 @@ static b32 setMessage(msg* const m, const void* data, i32 size) {
     
     if (m->pData = alloc(size), !m->pData) { return 0; }
     
-    memcpy(m->pData, data, sizeof size);
+    memcpy(m->pData, data, size);
     return 1;
 }
 
@@ -110,17 +110,17 @@ exception remove_mailbox(mailbox* mBox) {
     return OK;
 }
 
-static inline b32 msgIsWaiting(const mailbox* mBox) { return mBox->nBlockedMsg < 0; }
+static inline b32 msgRecIsWaiting(const mailbox* mBox) { return mBox->nBlockedMsg < 0; }
+static inline b32 msgSndIsWaiting(const mailbox* mBox) { return mBox->nBlockedMsg > 0; }
 
 // NOTE: not tested
 exception send_wait(mailbox* mBox, void* pData) {
     volatile int first = TRUE;
     isr_off();
     SaveContext();
-    
     if (first) {
         first = FALSE;
-        if (msgIsWaiting(mBox)) {
+        if (msgRecIsWaiting(mBox)) {
             setMessage(getFirstMsg(mBox), pData, getDataSize(mBox));
             msg* rec = msgPopFront(mBox);
             addTask_Deadline(readyList, getTask(rec));
@@ -150,10 +150,9 @@ exception receive_wait(mailbox* mBox, void* pData) {
     volatile int first = TRUE;
     isr_off();
     SaveContext();
-    
     if (first) {
         first = FALSE;
-        if (mBox->nBlockedMsg > 0) {
+        if (msgSndIsWaiting(mBox)) {
             memcpy(pData, getFirstMsg(mBox)->pData, getDataSize(mBox));
             msg* snd = msgPopFront(mBox);
             if (snd->pBlock != NULL) {
@@ -182,132 +181,58 @@ exception receive_wait(mailbox* mBox, void* pData) {
     }
 }
 
-/*
-// NOTE: TEST!!
-exception send_wait(mailbox* mBox, void* pData) {
-    volatile int isFirst = TRUE;
-    isr_off();
-    SaveContext();
-    if(isFirst) {
-        isFirst = FALSE;
-        // IF receiving task is waiting THEN
-        if(mBox->pHead->Status == RECEIVER) {
-            // Copy sender's data to the data area of the receivers Message
-            memcpy(mBox->pHead->pData, pData, sizeof (mBox->nDataSize));
-            // Remove receiving task's Message struct from the mailbox;
-            TaskNode* task = mBox->pHead->pBlock;
-            msg* m = removeMsg(firstNode)
-            delete_msg(mBox, mBox->pHead);
-            // Move receiving task to Readylist
-            addTask_Deadline(&readyList, removeTask(task));
-        } else {
-            create_msg_first(mBox, pData);
-            // Move sending task from Readylist to Waitinglist
-            addTask_Deadline(&waitList, removeTask(mBox->pHead->pBlock));
-        }
-        LoadContext();
-    } else {
-        // IF deadline is reached THEN
-        if (msgDeadLine(mBox->pHead)) {
-            //isr_off();
-            // Remove send Message
-            delete_msg(mBox, mBox->pHead);
-            //isr_on();
-            return DEADLINE_REACHED;
-        }
-    }
-    return OK;
-}
 
-// NOTE: TEST!!
-exception receive_wait(mailbox* mBox, void* pData) {
-    volatile int isFirst = TRUE;
-    isr_off();
-    SaveContext();
-    if(isFirst) { 
-        isFirst = FALSE;
-        // IF send Message is waiting THEN
-        if(mBox->pHead->Status == SENDER) {
-            // Copy sender's data to receiving task's data area
-            memcpy(pData, mBox->pHead, mBox->nDataSize);
-            // Remove sending task's Message struct from the Mailbox
-            TaskNode* task = mBox->pHead->pBlock;
-            uint status = mBox->pHead->Status;
-            delete_msg(mBox, mBox->pHead);
-            // IF Message was of wait type THEN
-            if (status == 0) {
-                // Move sending task to Ready list
-            } else {
-                // Free senders data area
-                delete(mBox->pHead->pData);
-            }
-        } else {
-            // Allocate a Message structure
-            msg* m = alloc_msg(pData);
-            //m->pTask = mBox->pHead->pBlock->pTask;
-            // Add Message to the Mailbox
-            add_msg_first(mBox, m);
-            // Move receiving task from Readylist to Waitinglist
-            // addTask_Deadline(&waitList, removeTask(readyList, m->pTask));
-        }
-        LoadContext();
-    } else {
-        // IF deadline is reached THEN
-        if (mBox->pHead->pBlock->pTask->DeadLine == 0) {
-            //isr_off();
-            delete_msg(mBox, mBox->pHead);
-            //isr_on();
-            return DEADLINE_REACHED;
-        } 
-    }
-    return OK;
-}
 
-// NOTE: TEST!!
+// NOTE: not tested
 exception send_no_wait(mailbox* mBox, void* pData) {
-    volatile int isFirst = TRUE;
+    volatile int first = TRUE;
     isr_off();
     SaveContext();
-    if (isFirst) {
-        isFirst = FALSE;
-        // IF receiving task is waiting THEN
-        if (mBox->pHead->Status == 0)  {
-            // Copy data to receiving task's data area.
-            // Remove receiving task's Message struct from the Mailbox
-            // Move receiving task to Readylist
+    if (first) {
+        first = FALSE;
+        if (msgRecIsWaiting(mBox))  {
+            setMessage(getFirstMsg(mBox), pData, getDataSize(mBox));
+            msg* rec = msgPopFront(mBox);
+            addTask_Deadline(readyList, getTask(rec));
+            deleteMsg(rec);
             LoadContext();
         } else {
+            msg* new = createMsg(pData); if (!new) { return FAIL; }
             if(isFull(mBox)) {
-                delete_msg_last(mBox);
+                deleteMsg(msgPopFront(mBox));
             }
-            create_msg_first(mBox, pData);
+            msgPushBack(mBox, new);
         }
     }
     return OK;
 }
 
-// NOTE: TEST!!
+// NOTE: not tested
 int receive_no_wait(mailbox* mBox, void* pData) {
-    volatile int isFirst = TRUE;
+    volatile int first = TRUE;
     isr_off();
     SaveContext();
-    if (isFirst)  {
-        isFirst = FALSE;
-        // IF send Message is waiting THEN
-        if (0) {
-            // Copy sender's data to receiving task's data area
-            // Remove sending task's Message struct from the Mailbox
-            // IF Message was of wait type THEN
-            if (0) {
-                // Move sending task to Readylist
-            } else  {
-                // Free senderís data area
-            }       
-        }
+    if (first)  {
+        first = FALSE;
+        if (msgSndIsWaiting(mBox)) {
+            memcpy(pData, getFirstMsg(mBox)->pData, getDataSize(mBox));
+            msg* snd = msgPopFront(mBox);
+            if (snd->pBlock != NULL) {
+                addTask_Deadline(readyList, snd->pBlock);
+                Running = getFirstNode(readyList)->pTask;
+                deleteMsg(snd);
+            } else {
+                deleteMsg(snd);
+            }
+        } else {
+            msg* new = createMsg(pData); if (!new) { return FAIL; }
+            msgPushBack(mBox, new);
+            addTask_Deadline(readyList, removeNode(firstNode(waitList)));
+            Running = firstNode(readyList)->pTask;
+        }     
         LoadContext();
     }
     return OK;
 }
-*/
 
 #endif
